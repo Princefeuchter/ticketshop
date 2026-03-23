3265f79f14084098452e11448877093f219341dd
imfley <grickscheit.dev@gmail.com>
2026-03-23 13:54:54 +0100

    impl(web): Added basics to index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+    title: 'Home',
+    description: 'Hol dir dein Ticket JETZT!',
+    navOrder: 1,
+    type: 'primary',
+    icon: 'i-mdi-home-variant'
+    })
+</script>
+
 <template>
     <div>
-        <h1>
+        <h1 class="text-4xl font-bold text-center mt-10">
             Leveste Feste - Ticketshop
         </h1>
     </div>
