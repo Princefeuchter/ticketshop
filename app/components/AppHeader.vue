c88b8fe4186cf70c1079708d9ccc3a4fdd3a0b26
imfley <grickscheit.dev@gmail.com>
2026-03-23 23:39:21 +0100

    feat(web): Added Basic Header

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav>
+      <ul class="flex space-x-4">
+        <li>
+          <NuxtLink to="/" class="text-gray-700 hover:text-gray-900">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/tickets" class="text-gray-700 hover:text-gray-900">Tickets</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/contact" class="text-gray-700 hover:text-gray-900">Contact</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
