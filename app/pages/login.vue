f6259142248e9520d81ec6a09579f71d9f28c305
imfley <grickscheit.dev@gmail.com>
2026-03-24 18:43:28 +0100

    feat(web): implemented basic login form

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -15,13 +15,24 @@
     <h1 class="text-4xl font-bold">Du bist nicht eingeloggt</h1>
     <p class="mt-4">Bitte logge dich ein, um deine Tickets zu verwalten.</p>
     <div class="flex justify-center mt-6">
-      <
-      <button @click="openInPopup('/auth/login')" class="px-4 py-2 bg-blue-500 text-white rounded">Login</button>
+      <UForm :schema="login" :state="state" class="space-y-4" @submit="onSubmit">
+        <UFormField class="" label="Email">
+          <UInput v-model="state.email" type="email" placeholder="Deine Email" />
+        </UFormField>
+        <UFormField class="mt-4" label="Password">
+          <UInput v-model="state.password" type="password" placeholder="Dein Passwort" />
+        </UFormField>
+        <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded">Login</button>
+
+      </UForm>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { FormSubmitEvent } from '@nuxt/ui'
+import * as z from 'zod'
+
 const {loggedIn, user, session, fetch, clear, openInPopup} = useUserSession()
 
 definePageMeta({
@@ -31,6 +42,29 @@ definePageMeta({
     type: 'primary',
     icon: 'i-mdi-account'
 })
+
+const login = z.object({
+  email: z.email(),
+  password: z.string().min(6, 'Passwort muss mindestens 6 Zeichen lang sein')
+})
+
+const state = reactive({
+  email: '',
+  password: ''
+})
+
+type Schema = z.infer<typeof login>
+
+const toast = useToast()
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  toast.add({
+    title: 'Login',
+    description: 'Login erfolgreich!',
+    color: 'success'
+  })
+  console.log(event.data)
+}
 </script>
 
 <style>
