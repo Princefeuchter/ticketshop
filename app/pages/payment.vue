26771fe662ace78d3258d3c3a2b6c1858d3cd3c5
imfley <grickscheit.dev@gmail.com>
2026-03-27 01:17:07 +0100

    feat(web): implemented payment page

---
+++ app/pages/payment.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+  definePageMeta({
+    title: 'Zahlung',
+    description: 'Hier kannst du deine Zahlung verwalten.',
+    navOrder: 5,
+    type: 'primary',
+    icon: 'i-mdi-credit-card'
+  })
+</script>
+
+<style>
+
+</style>
